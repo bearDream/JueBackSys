5b407969129b624006d81185524edcc7f374871c
yang <123@qq.c0m>
2017-05-16 19:49:23 +0800

    商家图片

--- src/app/Business/businesstype/index.vue
+++ src/app/Business/businesstype/index.vue
@@ -16,6 +16,8 @@
       <div>
         <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
           <Form-item label="创建商家id" prop="businessTypeId">
+
+
             <Row>
               <i-col span="18">
                 <Input v-model="formValidate.businessTypeId" placeholder="请输入的商家ID"></Input>
@@ -40,13 +42,48 @@
           </Form-item>
 
 
-          <Button type="success" long @click="handleSave('formValidate')">保存</Button>
         </Form>
       </div>
       <!-- 表单 -->
-
       <div slot="footer">
+        <Button type="success" long @click="handleSave('formValidate')">保存</Button>
       </div>
+
+      <p>上传图片</p>
+      <div class="demo-upload-list" v-for="item in uploadList">
+        <template v-if="item.status === 'finished'">
+          <img :src="item.url">
+          <div class="demo-upload-list-cover">
+            <Icon type="ios-eye-outline" @click.native="handleView(item.name)"></Icon>
+            <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
+          </div>
+        </template>
+        <template v-else>
+          <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
+        </template>
+      </div>
+      <Upload
+        ref="upload"
+        :show-upload-list="false"
+        :default-file-list="defaultList"
+        :on-success="handleSuccess"
+        :format="['jpg','jpeg','png']"
+        :max-size="2048"
+        :on-format-error="handleFormatError"
+        :on-exceeded-size="handleMaxSize"
+        :before-upload="handleBeforeUpload"
+        multiple
+        type="drag"
+        action="//jsonplaceholder.typicode.com/posts/"
+        style="display: inline-block;width:58px;">
+        <div style="width: 58px;height:58px;line-height: 58px;">
+          <Icon type="camera" size="20"></Icon>
+        </div>
+      </Upload>
+      <Modal title="查看图片" v-model="visible">
+        <img :src="'https://o5wwk8baw.qnssl.com/' + businessImage + '/large'" v-if="visible" style="width: 100%">
+      </Modal>
+
     </Modal>
 
     <Modal
@@ -115,6 +152,7 @@
   import consts from '@/utils/consts'
   import time from '@/utils/helpers/timeLite'
   import List, { ListHeader, ListOperations, ListSearch } from '@/components/List'
+  import Editor from '@/components/Editor'
 
   export default {
     name: 'businesstype',
@@ -122,10 +160,24 @@
       List,
       ListHeader,
       ListOperations,
-      ListSearch
+      ListSearch,
+      Editor
     },
     data () {
       return {
+        defaultList: [
+          {
+            'name': 'a42bdcc1178e62b4694c830f028db5c0',
+            'url': 'https://o5wwk8baw.qnssl.com/a42bdcc1178e62b4694c830f028db5c0/avatar'
+          },
+          {
+            'name': 'bc7521e033abdd1e92222d733590f104',
+            'url': 'https://o5wwk8baw.qnssl.com/bc7521e033abdd1e92222d733590f104/avatar'
+          }
+        ],
+        imgName: '',
+        visible: false,
+        uploadList: [],
         businessTypeId: '',
         typeName: '',
         parentId: '',
@@ -242,6 +294,41 @@
       this.get()
     },
     methods: {
+      handleView (name) {
+        this.businessImage = name
+        this.visible = true
+      },
+      handleRemove (file) {
+        // 从 upload 实例删除数据
+        const fileList = this.$refs.upload.fileList
+        this.$refs.upload.fileList.splice(fileList.indexOf(file), 1)
+      },
+      handleSuccess (res, file) {
+        // 因为上传过程为实例，这里模拟添加 url
+        file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar'
+        file.name = '7eb99afb9d5f317c912f08b5212fd69a'
+      },
+      handleFormatError (file) {
+        this.$Notice.warning({
+          title: '文件格式不正确',
+          desc: '文件 ' + file.name + ' 格式不正确，请上传 jpg 或 png 格式的图片。'
+        })
+      },
+      handleMaxSize (file) {
+        this.$Notice.warning({
+          title: '超出文件大小限制',
+          desc: '文件 ' + file.name + ' 太大，不能超过 2M。'
+        })
+      },
+      handleBeforeUpload () {
+        const check = this.uploadList.length < 5
+        if (!check) {
+          this.$Notice.warning({
+            title: '最多只能上传 5 张图片。'
+          })
+        }
+        return check
+      },
       get (current = 1) {
         this.$set(this, 'current', current)
         this.$store.dispatch('getBusinesstypes', {
@@ -317,6 +404,47 @@
       resetFields () {
         this.$refs.formValidate.resetFields()
       }
+    },
+    mounted () {
+      this.uploadList = this.$refs.upload.fileList
     }
   }
 </script>
+<style>
+  .demo-upload-list{
+    display: inline-block;
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    overflow: hidden;
+    background: #fff;
+    position: relative;
+    box-shadow: 0 1px 1px rgba(0,0,0,.2);
+    margin-right: 4px;
+  }
+  .demo-upload-list img{
+    width: 100%;
+    height: 100%;
+  }
+  .demo-upload-list-cover{
+    display: none;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0,0,0,.6);
+  }
+  .demo-upload-list:hover .demo-upload-list-cover{
+    display: block;
+  }
+  .demo-upload-list-cover i{
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+    margin: 0 2px;
+  }
+</style>
