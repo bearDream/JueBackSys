40c218b13d704a11e61482896398f3d4694d7140
杨娇娇 <23423@qq.com>
2017-05-17 18:37:54 +0800

    用户管理yangjiaojiao

--- src/router/routes/user_frame.js
+++ src/router/routes/user_frame.js
@@ -1,4 +1,10 @@
 export default {
   path: 'userFrame',
-  component: resolve => require(['@/app/Base'], resolve)
+  component: resolve => require(['@/app/User'], resolve),
+  children: [
+    {
+      path: '/',
+      component: resolve => require(['@/app/User/List'], resolve)
+    }
+  ]
 }
